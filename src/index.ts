2bdf2fdc61a97ed733265bc87fa284a335344d56
siimsep <siimsep@tlu.ee>
2022-01-19 13:10:23 +0200

    API doc

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
 import express, { Express } from 'express';
 import usersController from './components/users/controller';
+import jobController from './components/jobs/controller';
+import swaggerUi from 'swagger-ui-express';
+import openapi from "./openapi.json";
+import cors from "cors";
 
 const app: Express = express();
+app.use(cors());
 app.use(express.json())
 const port: number = 3000;
-import cors from "cors";
-import jobController from './components/jobs/controller';
-
-app.use(cors());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(openapi));
 /////////////////////////////////////////////////////
 //  USERS
 app.get("/users", usersController.getAllUsers);
 app.get("/users/:id", usersController.getUserbyId);
 app.post("/users", usersController.addUser);
 app.delete("/users/:id", usersController.deleteUser);
-
-
 // JOBS
 app.get("/jobs", jobController.getAllJobs);
 app.get("/jobs/:id", jobController.getAJob);
@@ -23,9 +23,6 @@ app.post("/jobs", jobController.addAJob);
 app.delete("/jobs/:id", jobController.deleteJob);
 app.patch("/jobs/:id", jobController.updateJob);
 /////////////////////////////////////////////////////
-
-
 app.listen(port, () => {
-  // eslint-disable-next-line no-console
   console.log(`App is running on port ${port}`);
 });
